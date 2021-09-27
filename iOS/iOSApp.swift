0ed6950adacdf3dafee9f04f9a651d9118a9effe
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-09-26 22:00:09 -0400

    added iOS target

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Vincent Cloutier on 2021-09-26.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
